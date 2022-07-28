8d70374c59e1230a750f9b187da056948d5b511a
Eden Avidan <edenaavidan@gmail.com>
2022-07-28 19:00:21 +0300

    added accurate distance from stays

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from 'react';
 import './assets/scss/global.scss';
 import DestinationList from './cmps/DestinationList';
 import MainHeader from './cmps/MainHeader';
-import {useStayStore} from './stores/stay-context';
+import { useStayStore } from './stores/stay-context';
 
 const App = () => {
-  const {stays} = useStayStore();
+  const { stays } = useStayStore();
+  const [userLocation, setUserLocation] = useState({ lat: 0, long: 0 });
+
+  const initGeoLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, fail);
+    }
+  }
+
+  const success = ({ coords }) => {
+    setUserLocation({ lat: coords.latitude, long: coords.longitude })
+  }
+
+  const fail = () => {
+    console.log('Failed to connect to geolocation services.');
+  }
+
+  useEffect(() => {
+    initGeoLocation();
+  }, [])
+
   return (
     <>
       <MainHeader />
-      <DestinationList stayList={stays} />
+      <DestinationList stayList={stays} userLocation={userLocation} />
     </>
   );
 };
--- src/cmps/DestinationPreview.jsx
+++ src/cmps/DestinationPreview.jsx
@@ -1,16 +1,21 @@
 import StarRating from '../assets/svgs/star-rating.svg';
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { utilService } from '../services/util-service';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const DestinationPreview = ({ stay }) => {
-    const dist = useRef(utilService.getRandomInt(1, 10000));
+const DestinationPreview = ({ stay, userLocation }) => {
+    // const dist = useRef(utilService.getRandomInt(1, 10000));
+
+    const getStayDistance = ({ lat, lan }) => {
+        const dist = utilService.getCoordsDistInKm(userLocation.lat, userLocation.long, lan, lat);
+        return utilService.numberWithCommas(parseInt(dist));
+    }
 
     const getFormattedRating = (num) => {
         if (!num) return 'New'
-        num /= 20;
+        num /= 20; // changing from 1/100 to 1/5 rating
         return num % 1 === 0 ? `${num}.0` : num;
     }
 
@@ -19,16 +24,18 @@ const DestinationPreview = ({ stay }) => {
         <section className='dest-preview'>
             <Slider dots={true} infinite={false}>
                 {
-                    stay.imgUrls.map((imgUrl, idx) => <div key={idx} className="dest-preview-img-container"><img src={require(`../assets/images/${imgUrl}`)} alt="" /></div>)
+                    stay.imgUrls.map((imgUrl, idx) => (
+                        <div key={idx} className="dest-preview-img-container">
+                            <img src={require(`../assets/images/${imgUrl}`)} alt="" />
+                        </div>
+                    ))
                 }
             </Slider>
 
             <div className='info-container'>
                 <section className="dest-preview-info">
-                    <p className="bold-text">
-                        {stay.address.city}, {stay.address.country}
-                    </p>
-                    <p className="light-text">{utilService.numberWithCommas(dist.current)} kilometers away</p>
+                    <p className="bold-text">{stay.address.city}, {stay.address.country}</p>
+                    <p className="light-text">{getStayDistance(stay.address.location)} kilometers away</p>
                     <p className="light-text">Sep 20 - 25</p>
                     <p><span className='dest-preview-price bold-text'>${stay.price}</span> night</p>
                 </section>
