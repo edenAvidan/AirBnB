265bc739b8d96185cd327232cc85b5c879796608
Eden Avidan <edenaavidan@gmail.com>
2022-07-28 15:43:48 +0300

    limited number of home page destinations

--- src/cmps/DestinationList.jsx
+++ src/cmps/DestinationList.jsx
@@ -1,10 +1,12 @@
 import DestinationPreview from './DestinationPreview.jsx';
 
-const DestinationList = ({stayList}) => {
+const DestinationList = ({ stayList }) => {
+  const stayListForRender = stayList.slice(0, 50);
+
   return (
     <section className="dest-list main-layout">
       <ul className="clean-list">
-        {stayList.map((stay) => (
+        {stayListForRender.map((stay) => (
           <DestinationPreview stay={stay} key={stay._id} />
         ))}
       </ul>
