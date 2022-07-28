795673d20fb7e2309fb0b1db2c980cc596af1140
Eden Avidan <edenaavidan@gmail.com>
2022-07-28 14:18:27 +0300

    added rating stars and image carousel

--- src/cmps/DestinationPreview.jsx
+++ src/cmps/DestinationPreview.jsx
@@ -1,3 +1,6 @@
+import StarRating from '../assets/svgs/star-rating.svg';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import { Carousel } from 'react-responsive-carousel';
 import { useState } from 'react';
 import { utilService } from '../services/util-service';
 
@@ -8,24 +11,50 @@ const DestinationPreview = ({ stay }) => {
         return Math.floor(Math.random() * (max - min) + min);
     };
 
-    const [currImgPos, setCurrImgPos] = useState(0);
+    // const [currImgPos, setCurrImgPos] = useState(0);
 
-    const switchImage = () => {
-        if (currImgPos + 1 < stay.imgUrls.length) setCurrImgPos(prevImgPos => prevImgPos + 1);
-        else setCurrImgPos(0);
-    }
+    // const switchImage = () => {
+    //     if (currImgPos + 1 < stay.imgUrls.length) setCurrImgPos(prevImgPos => prevImgPos + 1);
+    //     else setCurrImgPos(0);
+    // }
 
     const getFormattedRating = (num) => {
-        num /= 20;
+        if (!num) return 'New'
         return num % 1 === 0 ? `${num}.0` : num;
     }
+
+    const handleChange = (index, item) => {
+        console.log(index, item)
+    }
+
+    const getConfigurableProps = () => ({
+        showThumbs: false,
+        autoPlay: false,
+        dynamicHeight: false,
+        showStatus: false,
+        showIndicators: false,
+        useKeyboardArrows: true,
+        verticalSwipe: 'natural',
+        onChange: handleChange
+    });
 
     if (!stay) return <div>Loading...</div>;
     return (
         <section className="dest-preview" >
-            <div className="dest-preview-img-container" onClick={switchImage}>
-                <img src={require(`../assets/images/${stay.imgUrls[currImgPos]}`)} />
-            </div>
+            {/* <div className="dest-preview-img-container" onClick={switchImage}>
+                <img src={require(`../assets/images/${stay.imgUrls[currImgPos]}`)} alt="" />
+            </div> */}
+
+            <Carousel {...getConfigurableProps()}>
+                {
+                    stay.imgUrls.map((imgUrl, idx) => (
+                        <div key={idx} className="dest-preview-img-container">
+                            <img src={require(`../assets/images/${imgUrl}`)} alt="" />
+                        </div>
+                    ))
+                }
+            </Carousel>
+
             <div className='info-container'>
                 <section className="dest-preview-info">
                     <p className="bold-text">
@@ -35,7 +64,11 @@ const DestinationPreview = ({ stay }) => {
                     <p className="light-text">Sep 20 - 25</p>
                     <p><span className='dest-preview-price bold-text'>${stay.price}</span> night</p>
                 </section>
-                <span className="dest-preview-rating">* {getFormattedRating(stay.reviewScores.rating)}</span>
+
+                <div className="dest-preview-rating flex">
+                    <img src={StarRating} className="rating-img" alt="" />
+                    <span>{getFormattedRating(stay.reviewScores.rating)}</span>
+                </div>
             </div>
         </section>
     );
