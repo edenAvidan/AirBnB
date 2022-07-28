0cd134f40465517e9a42017f722bebab445c252f
dvir <dvir6g6@gmail.com>
2022-07-28 16:06:09 +0300

    helpers

--- src/cmps/MainHeader.jsx
+++ src/cmps/MainHeader.jsx
@@ -4,9 +4,9 @@ const MainHeader = () => {
     <div className="main-header">
       <section className="container flex justify-between align-center main-layout">
         <div className="logo">airbnb</div>
-        <div className="search-bar flex justify-between align-center">
+        <div className="search-bar flex justify-around align-center">
           <div>Anywhere</div>
-          <div>Any Week</div>
+          <div>Any week</div>
           <div>Add guests</div>
           <div className="search-icon flex align-center justify-center">
             <img src={search} alt="" />
