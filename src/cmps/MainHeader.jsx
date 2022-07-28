32256b2f3dfaa37d140dfcd81240648257510563
dvir <dvir6g6@gmail.com>
2022-07-28 12:03:08 +0300

    util-service added

--- src/cmps/MainHeader.jsx
+++ src/cmps/MainHeader.jsx
@@ -1,5 +1,13 @@
 const MainHeader = () => {
-  return <div className="main-header">MainHeader</div>;
+  return (
+    <div className="main-header flex justify-between align-center">
+      <div className="logo">airbnb</div>
+      <div className="search-bar">
+        <input type="text" />
+      </div>
+      <div className="log-in">Login</div>
+    </div>
+  );
 };
 
 export default MainHeader;
