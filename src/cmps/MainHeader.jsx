0d4fa253a7cb05b1855940d68fd43c03cde646c2
dvir <dvir6g6@gmail.com>
2022-07-28 14:14:11 +0300

    search-bar styling

--- src/cmps/MainHeader.jsx
+++ src/cmps/MainHeader.jsx
@@ -1,14 +1,18 @@
+import search from '../assets/svgs/search.svg';
 const MainHeader = () => {
   return (
     <div className="main-header">
       <section className="container flex justify-between align-center main-layout">
         <div className="logo">airbnb</div>
-        <div className="search-bar flex justify-between">
+        <div className="search-bar flex justify-between align-center">
           <div>Anywhere</div>
           <div>Any Week</div>
           <div>Add guests</div>
+          <div className="search-icon flex align-center justify-center">
+            <img src={search} alt="" />
+          </div>
         </div>
-        <div className="log-in">Login</div>
+        <div className="log-in flex align-center">Login</div>
       </section>
     </div>
   );
