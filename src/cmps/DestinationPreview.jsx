52591641b084bc2e8d8d6fc21b9767f45ae7558e
Eden Avidan <edenaavidan@gmail.com>
2022-07-28 20:48:56 +0300

    disabled preview dots navigation. added like button to preview

--- src/cmps/DestinationPreview.jsx
+++ src/cmps/DestinationPreview.jsx
@@ -1,28 +1,32 @@
 import StarRating from '../assets/svgs/star-rating.svg';
-import { useRef } from 'react';
+import { ReactComponent as LikeIcon } from '../assets/svgs/favorite.svg';
+import { useEffect } from 'react';
 import { utilService } from '../services/util-service';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const DestinationPreview = ({ stay, userLocation }) => {
-    // const dist = useRef(utilService.getRandomInt(1, 10000));
-
     const getStayDistance = ({ lat, lan }) => {
         const dist = utilService.getCoordsDistInKm(userLocation.lat, userLocation.long, lan, lat);
         return utilService.numberWithCommas(parseInt(dist));
     }
 
     const getFormattedRating = (num) => {
-        if (!num) return 'New'
-        num /= 20; // changing from 1/100 to 1/5 rating
+        if (!num) return 'New';
+        num /= 20; // changing from 1/100 to 1/5 rating.
         return num % 1 === 0 ? `${num}.0` : num;
     }
+
+    useEffect(() => {
+        const elDots = document.querySelectorAll('.slick-dots li button');
+        Array.from(elDots).map(dot => dot.disabled = true);
+    }, [])
 
     if (!stay) return <div>Loading...</div>;
     return (
         <section className='dest-preview'>
-            <Slider dots={true} infinite={false}>
+            <Slider dots={true} infinite={false} draggable={false}>
                 {
                     stay.imgUrls.map((imgUrl, idx) => (
                         <div key={idx} className="dest-preview-img-container">
@@ -31,6 +35,7 @@ const DestinationPreview = ({ stay, userLocation }) => {
                     ))
                 }
             </Slider>
+            <LikeIcon className="like-icon" />
 
             <div className='info-container'>
                 <section className="dest-preview-info">
