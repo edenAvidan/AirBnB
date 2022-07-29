43af2b518a76e2fbf6a515f9f401b74cbdc03bcd
Eden Avidan <edenaavidan@gmail.com>
2022-07-29 10:31:46 +0300

    misc

--- src/cmps/DestinationPreview.jsx
+++ src/cmps/DestinationPreview.jsx
@@ -1,57 +1,67 @@
-import StarRating from '../assets/svgs/star-rating.svg';
-import { ReactComponent as LikeIcon } from '../assets/svgs/favorite.svg';
-import { useEffect } from 'react';
-import { utilService } from '../services/util-service';
+import StarRating from "../assets/svgs/star-rating.svg";
+import { ReactComponent as LikeIcon } from "../assets/svgs/favorite.svg";
+import { useEffect } from "react";
+import { utilService } from "../services/util-service";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const DestinationPreview = ({ stay, userLocation }) => {
-    const getStayDistance = ({ lat, lan }) => {
-        const dist = utilService.getCoordsDistInKm(userLocation.lat, userLocation.long, lan, lat);
-        return utilService.numberWithCommas(parseInt(dist));
-    }
-
-    const getFormattedRating = (num) => {
-        if (!num) return 'New';
-        num /= 20; // changing from 1/100 to 1/5 rating.
-        return num % 1 === 0 ? `${num}.0` : num;
-    }
-
-    useEffect(() => {
-        const elDots = document.querySelectorAll('.slick-dots li button');
-        Array.from(elDots).map(dot => dot.disabled = true);
-    }, [])
-
-    if (!stay) return <div>Loading...</div>;
-    return (
-        <section className='dest-preview'>
-            <Slider dots={true} infinite={false} draggable={false}>
-                {
-                    stay.imgUrls.map((imgUrl, idx) => (
-                        <div key={idx} className="dest-preview-img-container">
-                            <img src={require(`../assets/images/${imgUrl}`)} alt="" />
-                        </div>
-                    ))
-                }
-            </Slider>
-            <LikeIcon className="like-icon" />
-
-            <div className='info-container'>
-                <section className="dest-preview-info">
-                    <p className="bold-text">{stay.address.city}, {stay.address.country}</p>
-                    <p className="light-text">{getStayDistance(stay.address.location)} kilometers away</p>
-                    <p className="light-text">Sep 20 - 25</p>
-                    <p><span className='dest-preview-price bold-text'>${stay.price}</span> night</p>
-                </section>
-
-                <div className="dest-preview-rating flex">
-                    <img src={StarRating} className="rating-img" alt="" />
-                    <span>{getFormattedRating(stay.reviewScores.rating)}</span>
-                </div>
-            </div>
-        </section>
+  const getStayDistance = ({ lat, lan }) => {
+    const dist = utilService.getCoordsDistInKm(
+      userLocation.lat,
+      userLocation.long,
+      lan,
+      lat
     );
+    return utilService.numberWithCommas(parseInt(dist));
+  };
+
+  const getFormattedRating = (num) => {
+    if (!num) return "New";
+    num /= 20; // changing from 1/100 to 1/5 rating.
+    return num % 1 === 0 ? `${num}.0` : num;
+  };
+
+  useEffect(() => {
+    const elDots = document.querySelectorAll(".slick-dots li button");
+    Array.from(elDots).map((dot) => (dot.disabled = true));
+  }, []);
+
+  if (!stay) return <div>Loading...</div>;
+  return (
+    <section className="dest-preview">
+      <Slider dots={true} infinite={false} draggable={false}>
+        {stay.imgUrls.map((imgUrl, idx) => (
+          <div key={idx} className="dest-preview-img-container">
+            <img src={require(`../assets/images/${imgUrl}`)} alt="" />
+          </div>
+        ))}
+      </Slider>
+      <LikeIcon className="like-icon" />
+
+      <div className="info-container">
+        <section className="dest-preview-info">
+          <p className="bold-text">
+            {stay.address.city}, {stay.address.country}
+          </p>
+          <p className="light-text">
+            {getStayDistance(stay.address.location)} kilometers away
+          </p>
+          <p className="light-text">Sep 20 - 25</p>
+          <p>
+            <span className="dest-preview-price bold-text">${stay.price}</span>{" "}
+            night
+          </p>
+        </section>
+
+        <div className="dest-preview-rating flex">
+          <img src={StarRating} className="rating-img" alt="" />
+          <span>{getFormattedRating(stay.reviewScores.rating)}</span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default DestinationPreview;
--- src/services/util-service.js
+++ src/services/util-service.js
@@ -4,25 +4,26 @@ export const utilService = {
   makeId,
   numberWithCommas,
   getRandomInt,
-  getCoordsDistInKm
-}
+  getCoordsDistInKm,
+};
 
 function saveToStorage(key, value) {
-  localStorage.setItem(key, JSON.stringify(value) || null)
+  localStorage.setItem(key, JSON.stringify(value) || null);
 }
 
 function loadFromStorage(key) {
-  let data = localStorage.getItem(key)
-  return data ? JSON.parse(data) : undefined
+  let data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : undefined;
 }
 
 function makeId(length = 8) {
-  var txt = ''
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  var txt = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   for (var i = 0; i < length; i++) {
-    txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    txt += possible.charAt(Math.floor(Math.random() * possible.length));
   }
-  return txt
+  return txt;
 }
 
 function numberWithCommas(x) {
@@ -30,21 +31,23 @@ function numberWithCommas(x) {
 }
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  const min = Math.ceil(min);
+  constmax = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
 }
 
 function getCoordsDistInKm(lat1, lon1, lat2, lon2) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = _deg2rad(lat2 - lat1);
-  var dLon = _deg2rad(lon2 - lon1);
-  var a =
+  const R = 6371; // Radius of the earth in km
+  const dLat = _deg2rad(lat2 - lat1);
+  const dLon = _deg2rad(lon2 - lon1);
+  const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(_deg2rad(lat1)) * Math.cos(_deg2rad(lat2)) *
-    Math.sin(dLon / 2) * Math.sin(dLon / 2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  var distInKm = R * c;
+    Math.cos(_deg2rad(lat1)) *
+      Math.cos(_deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distInKm = R * c;
   return distInKm;
 }
 
