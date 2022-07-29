4eac17441d02717a00b0ba971f3d0713306bae56
dvir <dvir6g6@gmail.com>
2022-07-29 11:10:34 +0300

    home-page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './assets/scss/global.scss';
 import MainHeader from './cmps/MainHeader';
 import {Routes, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
+
 const App = () => {
   return (
     <>
