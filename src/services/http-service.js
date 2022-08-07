bfc619adcb7e393e7b3cd90fb86d39ed88ae32c6
dvir <dvir6g6@gmail.com>
2022-08-07 23:26:33 +0300

    http service correction

--- src/services/http-service.js
+++ src/services/http-service.js
@@ -1,25 +1,26 @@
-import Axios from 'axios'
+import Axios from 'axios';
 
-const BASE_URL = process.env.NODE_ENV === 'production' ? '/api/' : '//localhost:3030/api/'
+const BASE_URL =
+  process.env.NODE_ENV === 'production' ? '/api/' : '//localhost:3030/api/';
 
 var axios = Axios.create({
   withCredentials: true,
-})
+});
 
 export const httpService = {
   get(endpoint, data) {
-    return ajax(endpoint, 'GET', data)
+    return ajax(endpoint, 'GET', data);
   },
   post(endpoint, data) {
-    return ajax(endpoint, 'POST', data)
+    return ajax(endpoint, 'POST', data);
   },
   put(endpoint, data) {
-    return ajax(endpoint, 'PUT', data)
+    return ajax(endpoint, 'PUT', data);
   },
   delete(endpoint, data) {
-    return ajax(endpoint, 'DELETE', data)
+    return ajax(endpoint, 'DELETE', data);
   },
-}
+};
 
 async function ajax(endpoint, method = 'GET', data = null) {
   try {
@@ -28,14 +29,16 @@ async function ajax(endpoint, method = 'GET', data = null) {
       method,
       data,
       params: method === 'GET' ? data : null,
-    })
-    return res.data
+    });
+    return res.data;
   } catch (err) {
-    console.log(`Had Issues ${method}ing to the backend, endpoint: ${endpoint}, with data: ${data}`)
-    console.dir(err)
+    console.error(
+      `Had Issues ${method}ing to the backend, endpoint: ${endpoint}, with data: ${data}`
+    );
+    console.dir(err);
     if (err.response && err.response.status === 401) {
-      router.push('/login')
+      router.push('/login');
     }
-    throw err
+    throw err;
   }
 }
